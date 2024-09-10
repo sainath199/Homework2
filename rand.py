cc796fdd3d53376b2ae39916a36cb8e86913c863
Monasree <71449495+Monasree@users.noreply.github.com>
2024-09-10 21:16:54 +0000

    added rand.py

--- rand.py
+++ rand.py
@@ -1,10 +1,21 @@
 import subprocess
 
-
 def random_array(arr):
-    shuffled_num = None
-    for i in range(len(arr)):
+    """
+    Fills the input array with random numbers by the 'shuf' command.
+
+    Args:
+        arr (list): The list to be filled with random integers.
+
+    Returns:
+        list: The input list filled with random integers.
+    """
+    for i, _ in enumerate(arr):
+        # Intentionally using subprocess without shell=True; no untrusted input is involved
+        # nosec: B603
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
+
+
